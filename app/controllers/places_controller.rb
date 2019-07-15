088435865151d9d5071af0fbd48bfe44a51051fe
Wes Griffin <typicalgames@gmail.com>
2019-07-15 21:56:34 +0000

    setup links in top nav

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,4 @@
 class PlacesController < ApplicationController
-
   def index
     @places = Place.all
   end
@@ -8,4 +7,14 @@ class PlacesController < ApplicationController
     @place = Place.new
   end
 
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
 end
