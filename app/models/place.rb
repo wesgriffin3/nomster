772282654afe1d05a6eb6b7f29d53f08a386e866
Wes Griffin <typicalgames@gmail.com>
2019-07-28 19:39:35 +0000

    geocode returns nil

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: true, length: {minimum: 3}
   validates :address, presence: true
   validates :description, presence: true
